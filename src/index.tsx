24864418a0c237996e25d7e27690dc66348a7f74
Adrian Wix <adrianwix18@gmail.com>
2023-06-14 11:42:34 +0200

    it is alive

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./styles.css";
 
-import App from "./CustomControls/App";
+import App from "./DataFlow/App";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement!);
